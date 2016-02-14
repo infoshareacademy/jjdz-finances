bdb08b7f0db533d73c1eedc38ea03e29fdbaa2ac
nairamk <nairamk@wp.pl>
2016-02-14 15:45:01 +0100

    Models classes added

--- src/main/java/com/infoshareacademy/finances/model/Fund.java
+++ src/main/java/com/infoshareacademy/finances/model/Fund.java
@@ -1,10 +1,46 @@
 package com.infoshareacademy.finances.model;
 
-/**
- * Created by uoli on 13.02.16.
- */
+import java.util.List;
+
 public class Fund {
 
+    private List<DailyValue> DailyValue;
+    private String fundName;
+    private String fundCode;
 
+    public Fund(List<DailyValue> DailyValue, String fundName, String fundCode) {
 
+        this.DailyValue = DailyValue;
+        this.fundName = fundName;
+        this.fundCode = fundCode;
+    }
+
+    public List<com.infoshareacademy.finances.model.DailyValue> getDailyValue() {
+        return DailyValue;
+    }
+
+    public void setDailyValue(List<com.infoshareacademy.finances.model.DailyValue> dailyValue) {
+        DailyValue = dailyValue;
+    }
+
+    public String getFundName() {
+        return fundName;
+    }
+
+    public void setFundName(String fundName) {
+        this.fundName = fundName;
+    }
+
+    public String getFundCode() {
+        return fundCode;
+    }
+
+    public void setFundCode(String fundCode) {
+        this.fundCode = fundCode;
+    }
+
+    @Override
+    public String toString() {
+        return DailyValue + " - " + fundName + " - " + fundCode;
+    }
 }
