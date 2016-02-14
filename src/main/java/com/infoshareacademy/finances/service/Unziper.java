e1d0e38f57f31278127fbbc75c4e27cd306b7cf3
LukaszGoc <lukaszgoc@yahoo.pl>
2016-02-14 16:37:31 +0100

    Working Unziper

--- src/main/java/com/infoshareacademy/finances/service/Unziper.java
+++ src/main/java/com/infoshareacademy/finances/service/Unziper.java
@@ -1,12 +1,56 @@
 package com.infoshareacademy.finances.service;
 
+import java.io.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
 /**
- * Created by uoli on 13.02.16.
+ * Do App.java wrzucić
+ *
+ *         Unziper unziper = new Unziper();
+ *         unziper.UnzipToFolder(zipFileName, outputFolderName);
+ *
  */
+
 public class Unziper {
 
-    public void UnzipToFolder(String zipFilePath, String outputFolderPath) {
+   // private static final String zipFileName = "Data/omegafun.zip";
+   // private static final String outputFolderName = "Data/Unziped";
+
+   // public static void main(String[] args) throws IOException {
+   //    Unziper unziper = new Unziper();
+   //    unziper.UnzipToFolder(zipFileName, outputFolderName);
+   // }
+
+    public void UnzipToFolder(String zipFilePath, String outputFolderPath) throws IOException {
+
+        byte[] buffer = new byte[2048];
+
+        ZipInputStream zipInput = new ZipInputStream(new FileInputStream(zipFilePath));
+        ZipEntry zipWorking = zipInput.getNextEntry();
+
+        while(zipWorking != null) {
+            String outputFileName = zipWorking.getName();
+            File outputFileFinal = new File(outputFolderPath + File.separator + outputFileName);
+            System.out.println("Unzipped file: " + outputFileName);
+
+            FileOutputStream fileOutputStream = new FileOutputStream(outputFileFinal);
+
+            for (int bufferSize = zipInput.read(buffer); bufferSize > 0; bufferSize = zipInput.read(buffer)) {
+                fileOutputStream.write(buffer, 0, bufferSize);
+            }
+
+            fileOutputStream.close();
+            zipWorking = zipInput.getNextEntry();
+        }
+
+        zipInput.closeEntry();
+        zipInput.close();
+
+        // System.out.println("Tadaaa");
 
     }
-
 }
+
+
+
