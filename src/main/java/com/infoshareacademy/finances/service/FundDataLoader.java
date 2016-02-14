1e28ba15b0c134e98d2b781f8625226e35c4b962
kzyskowski <karol.zyskowski1@gmail.com>
2016-02-14 15:37:41 +0100

    first implementation of data loader

--- src/main/java/com/infoshareacademy/finances/service/FundDataLoader.java
+++ src/main/java/com/infoshareacademy/finances/service/FundDataLoader.java
@@ -1,15 +1,61 @@
 package com.infoshareacademy.finances.service;
 
+import com.infoshareacademy.finances.model.DailyValue;
 import com.infoshareacademy.finances.model.Fund;
 
-/**
- * Created by uoli on 13.02.16.
- */
+import java.io.File;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.regex.Pattern;
+
 public class FundDataLoader {
 
-    public Fund loadDataFromFile(String filePath) {
+    public Fund loadDataFromFile(String filePath, Optional<Fund> fund) {
+        Path path = Paths.get(filePath);
+        Fund realFund = null;
+        Collection<DailyValue> dailyValues = new ArrayList<DailyValue>();
 
-        return null;
+        DateFormat format = new SimpleDateFormat("yyyyMMdd" , Locale.ENGLISH);
+
+        try {
+            List<String> lines = Files.readAllLines(path);
+            int size = lines.size();
+            for (int i = 1; i < size; i++) {
+                String[] fields = lines.get(i).split(",");
+                Date date = null;
+                try {
+                    date = format.parse(fields[1]);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                BigDecimal closeValue = new BigDecimal(fields[4]);
+                DailyValue dailyValue = new DailyValue(date, closeValue);
+                dailyValues.add(dailyValue);
+            }
+
+            if (fund.isPresent()) {
+                fund.setValues(dailyValues);
+                realFund = fund;
+            } else {
+                realFund = new Fund(dailyValues,"none","none");
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return realFund;
     }
+
+
 
 }
