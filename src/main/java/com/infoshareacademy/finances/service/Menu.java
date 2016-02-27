0ea4df2502b3c632eaa350bf2c69507afbe5a9a2
nairamk <nairamk@wp.pl>
2016-02-27 16:14:10 +0100

    Menu class (as main)

---
+++ src/main/java/com/infoshareacademy/finances/service/Menu.java
@@ -0,0 +1,12 @@
+package com.infoshareacademy.finances.service;
+
+import java.io.IOException;
+
+public class Menu {
+    public static void main(String[] argv) throws IOException {
+        String lstFile = argv[0];
+        System.out.println("lstFile = [" + lstFile + "]");
+
+    }
+
+}
