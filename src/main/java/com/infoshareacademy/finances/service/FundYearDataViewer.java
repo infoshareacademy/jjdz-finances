91a3ff8bf65509260a52a0457e68662b76727dde
LukaszGoc <lukaszgoc@yahoo.pl>
2016-02-28 18:38:18 +0100

    add year view & extrems FiX

--- src/main/java/com/infoshareacademy/finances/service/FundYearDataViewer.java
+++ src/main/java/com/infoshareacademy/finances/service/FundYearDataViewer.java
@@ -9,31 +9,37 @@ import java.util.*;
 
 public class FundYearDataViewer {
 
-
-    public static Set<String> showYears(Fund fund) {
+    public Set<String> showYears(Fund fund) {
         List<DailyValue> dailyData = fund.getDailyValues();
         Set<String> years = new HashSet<String>();
 
         for (DailyValue x: dailyData) {
-            String y = x.getDate().substring(0, 3);
+            String y = x.getDate().substring(0, 4);
             if (!years.contains(y)) years.add(y);
         }
         return years;
     }
 
-    public static List<BigDecimal> yearExtremes(Fund fund, String giveYear) {
+    public HashMap<String, BigDecimal> yearExtremes(Fund fund, String giveYear) {
         List<DailyValue> dailyData = fund.getDailyValues();
-        String year = giveYear;
+//        String year = giveYear;
         List<BigDecimal> yearValues = new ArrayList<BigDecimal>();
-        List<BigDecimal> minMax = new ArrayList<BigDecimal>();
+//        List<BigDecimal> minMax = new ArrayList<BigDecimal>();
+        HashMap<String, BigDecimal> minMax = new HashMap<String, BigDecimal>();
 
 
         for (DailyValue x: dailyData) {
-            if (x.getDate().substring(0, 3) == year) yearValues.add(x.getCloseValue());
+            if (x.getDate().substring(0, 4).equals(giveYear)) yearValues.add(x.getCloseValue());
 
             }
-        minMax.add(Collections.max(yearValues));
-        minMax.add(Collections.min(yearValues));
+
+        System.out.println("yearValues = " + yearValues);
+
+        minMax.put("Max", Collections.max(yearValues));
+        minMax.put("Min", Collections.min(yearValues));
+
+//        minMax.add(Collections.max(yearValues));
+//        minMax.add(Collections.min(yearValues));
 
         return minMax;
         }
