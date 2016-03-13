f1914d425b1165aedafc90af7db655a370e185dd
LukaszGoc <lukaszgoc@yahoo.pl>
2016-03-13 17:34:28 +0100

    loops for lambdas in FoundYearDataViewer, fixed test, cleaned Unziper.

--- src/test/java/com/infoshareacademy/finances/service/FundYearDataViewerTest.java
+++ src/test/java/com/infoshareacademy/finances/service/FundYearDataViewerTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 
@@ -17,7 +16,7 @@ import static org.junit.Assert.assertThat;
 public class FundYearDataViewerTest {
 
     @Test
-    public void testMinMaxValues() {
+    public void testMaxValues() {
 
         List<DailyValue> dailyValues = Arrays.asList(
                 new DailyValue(LocalDate.of(2009, 12, 12), new BigDecimal("12.1")),
@@ -27,10 +26,21 @@ public class FundYearDataViewerTest {
 
         List<DailyValue> out = viewer.yearMaxValues(dailyValues, 2009);
 
-//        HashMap<String, BigDecimal> map = viewer.yearExtremes(new Fund(dailyValues, "", ""), 2009);
-
         assertThat(out.get(0), equalTo(new DailyValue(LocalDate.of(2009, 12, 13), new BigDecimal("12.3"))));
-//        assertThat(out.get("Min"), is(equalTo(new BigDecimal("12.1"))));
+    }
+
+    @Test
+    public void testMinValues() {
+
+        List<DailyValue> dailyValues = Arrays.asList(
+                new DailyValue(LocalDate.of(2009, 12, 12), new BigDecimal("12.1")),
+                new DailyValue(LocalDate.of(2009, 12, 13), new BigDecimal("12.3")));
+
+        FundYearDataViewer viewer = new FundYearDataViewer();
+
+        List<DailyValue> out = viewer.yearMinValues(dailyValues, 2009);
+
+        assertThat(out.get(0), equalTo(new DailyValue(LocalDate.of(2009, 12, 12), new BigDecimal("12.1"))));
     }
 
 
