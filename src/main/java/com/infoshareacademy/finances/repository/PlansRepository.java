8661eefb14e639505e4ed6a9e6f6d2bbba5af75d
Lukasz <lukaszgoc@yahoo.pl>
2016-05-27 13:53:50 +0200

    Update PlansRepository.java
    More sample data to test stats.

--- src/main/java/com/infoshareacademy/finances/repository/PlansRepository.java
+++ src/main/java/com/infoshareacademy/finances/repository/PlansRepository.java
@@ -19,9 +19,12 @@ public class PlansRepository {
 	public List<PlanCreationDto> findAllPlans() {
 	//	return em.createQuery("select p from PlanCreationDto p", PlanCreationDto.class).getResultList();
 		List<PlanCreationDto> plansList = new ArrayList<>();
-		plansList.add(new PlanCreationDto(1L, ZonedDateTime.now(), new Asset("Azoty", "AZ001"), PlanCreationDto.PlanActionType.BUY, 67));
-		plansList.add(new PlanCreationDto(2L, ZonedDateTime.now(), new Asset("Koty", "DZ031"), PlanCreationDto.PlanActionType.BUY, 57));
-		plansList.add(new PlanCreationDto(3L, ZonedDateTime.now(), new Asset("Mloty", "BB021"), PlanCreationDto.PlanActionType.SELL, 26));
+		plansList.add(new PlanCreationDto(1L, ZonedDateTime.now(), new Asset("NOVO Akcji Globalnych", "OPE033"), PlanCreationDto.PlanActionType.BUY, 67));
+		plansList.add(new PlanCreationDto(2L, ZonedDateTime.now(), new Asset("NOVO Akcji Globalnych", "OPE033"), PlanCreationDto.PlanActionType.BUY, 57));
+		plansList.add(new PlanCreationDto(3L, ZonedDateTime.now(), new Asset("NOVO Zrownowazonego Wzrostu", "SEB001"), PlanCreationDto.PlanActionType.SELL, 26));
+		plansList.add(new PlanCreationDto(4L, ZonedDateTime.now(), new Asset("NOVO Akcji Globalnych", "OPE033"), PlanCreationDto.PlanActionType.BUY, 27));
+		plansList.add(new PlanCreationDto(5L, ZonedDateTime.now(), new Asset("ALLIANZ Akcji", "ALL001"), PlanCreationDto.PlanActionType.BUY, 17));
+		plansList.add(new PlanCreationDto(6L, ZonedDateTime.now(), new Asset("NOVO Zrownowazonego Wzrostu", "SEB001"), PlanCreationDto.PlanActionType.SELL, 25));
 		return plansList;
 	}
 }
